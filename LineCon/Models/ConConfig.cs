9c154c313e1fbcbb130e3086cede0e05ad4b5c22
antarctic-wolf <amarnerok@gmail.com>
2019-01-30 22:44:48 -0600

    remove unnecessary usings

--- LineCon/Models/ConConfig.cs
+++ LineCon/Models/ConConfig.cs
@@ -1,11 +1,7 @@
-﻿using LineCon.Data;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LineCon.Models
 {
