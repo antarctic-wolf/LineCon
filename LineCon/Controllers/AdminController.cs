2636aad3c0aec9eee8d106611608ccd82154c638
antarctic-wolf <amarnerok@gmail.com>
2019-02-02 17:13:37 -0600

    Started admin console

---
+++ LineCon/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using LineCon.Models.ViewModels.Admin;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineCon.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new ConConfigViewModel()
+            {
+                ConConfigId = Guid.Empty,
+                ConventionId = Guid.Empty,
+                RegistrationHours = new List<Tuple<DateTime, DateTime>>()
+                {
+                    new Tuple<DateTime, DateTime>(DateTime.Now, DateTime.Now.AddHours(8))
+                },
+                TicketWindowInterval = 5,
+                TicketWindowCapacity = 5,
+                RequireConfirmationNumber = true
+            });
+        }
+    }
+}
---
+++ LineCon/Models/ViewModels/Admin/ConConfigViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace LineCon.Models.ViewModels.Admin
+{
+    public class ConConfigViewModel
+    {
+        public Guid ConConfigId { get; set; }
+        public Guid ConventionId { get; set; }
+
+        [Display(Name = "Registration Hours of Operation")]
+        public List<Tuple<DateTime, DateTime>> RegistrationHours { get; set; }
+
+        [Display(Name = "Ticket Window Interval")]
+        public int TicketWindowInterval { get; set; }
+
+        [Display(Name = "Ticket Window Capacity")]
+        public int TicketWindowCapacity { get; set; }
+
+        [Display(Name = "Require Confirmation Number")]
+        public bool RequireConfirmationNumber { get; set; }
+    }
+}
